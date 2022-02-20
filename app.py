bd61964e034eb5f10bb70228c10c596299004b1b
Anastasia <pliskaanastasia1@gmail.com>
2022-02-21 00:52:04 +0300

    Commit correction 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from func import db_connect
 
 app = Flask(__name__)
@@ -6,18 +6,20 @@ app = Flask(__name__)
 @app.route('/<id>', methods=['GET'])
 def search_animal(id):
     query = f"""
-                SELECT animal_id, animal_type_id, name, breed_id, date_of_birth
+                SELECT animals_table(animal_id, animal_type_id, name, breed_id, date_of_birth)
                 FROM animals_table
                 WHERE "id" = {id}
+                LEFT JOIN animal_types ON animals_table.animal_type_id = animal_types.animal_type
+                LEFT JOIN breeds ON animals_table.breed_id = breeds.breed
                 """
     result = db_connect('animal.db', query)
     response = []
     for line in result:
         line_dict = {
             "animal_id": line[0],
-            "animal_type_id": line[1],
+            "animal_type": line[1],
             "name": line[2],
-            "breed_id": line[3],
+            "breed": line[3],
             "date_of_birth": line[4],
         }
         response.append(line_dict)
